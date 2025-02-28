240d7c4e90244d3427624cb3450da6ff70ba48b9
samzong <samzong.lu@gmail.com>
2025-02-28 17:19:11 +0800

    chore(font): use local font
    Signed-off-by: samzong <samzong.lu@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,18 @@ export default {
   darkMode: ["class"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'sans-serif',
+        ],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
